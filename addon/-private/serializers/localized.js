c76d82989e967deb4acdccaaa2d1a88e86af1a06
Florian Mäder <fkm@users.noreply.github.com>
2020-09-10 14:49:36 +0200

    fix(serializer): handle for non-localized models
    Otherwise, we are forced to use a different serializer for non-localized models.

--- addon/-private/serializers/localized.js
+++ addon/-private/serializers/localized.js
@@ -3,7 +3,10 @@ import JSONAPISerializer from "@ember-data/serializer/json-api";
 export default class ScopeSerializer extends JSONAPISerializer {
   serializeAttribute(snapshot, json, key, attributes) {
     super.serializeAttribute(snapshot, json, key, attributes);
-    if (snapshot.record.localizedFields.includes(key)) {
+
+    let { localizedFields = [] } = snapshot.record;
+
+    if (localizedFields.includes(key)) {
       json.attributes[key] = snapshot.record.getUnlocalizedField(key);
     }
   }
